3839f3209a9619817b3cfd66ac96f3a101c772bb
kedz-1 <khadar7@hotmail.co.uk>
2025-06-10 14:52:28 +0100

    Further tests written for the get end points, more specifically the get_issue_by_id

--- test/test_get.py
+++ test/test_get.py
@@ -61,3 +61,33 @@ def test_get_issues_returns_empty_list_when_no_post(client):
     
     assert response.status_code == 200
     assert response.json() == []
+
+
+def test_get_issue_by_id_returns_1_issue(client, example):
+    client.post('/issues', json=example)
+    
+    response2 = client.post('/issues', json={
+        'title': 'This is testing the get by ID',
+        'description': 'We are assigning an ID as ID is randomly generated on every invocation.'
+    })
+
+
+    created_id = response2.json()['id']
+
+    result = client.get(f'/issues/{created_id}')
+
+    response = client.get('/issues')
+
+    assert result.json()['title'] == 'This is testing the get by ID'
+    assert result.json()['description'] == 'We are assigning an ID as ID is randomly generated on every invocation.'
+    assert len(response.json()) == 2
+
+
+def test_get_issue_by_id_returns_404_when_id_not_found(client, example):
+    client.post('/issues', json=example)
+
+    response = client.get('/issues/1')
+
+
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Issue not found.'
