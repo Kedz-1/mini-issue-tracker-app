17e8662db9bc779376a8ab6e7e59165d4cad4ff6
kedz-1 <khadar7@hotmail.co.uk>
2025-07-24 19:41:08 +0100

    Tests written to ensure patch request is updating single field and saves to memory

--- test/test_patch.py
+++ test/test_patch.py
@@ -17,9 +17,34 @@ def example():
     }
 
 
-def test_update_issue_returns_404_on_non_existent_issue(client, example):
+def test_update_issue_raises_404_on_non_existent_issue(client, example):
     result = client.post('/issues', json=example)
 
     response = client.patch('/issues/this_id_does_not_exist', json={'status':'Closed'})
 
     assert response.status_code == 404 
+    assert response.json()['detail'] == 'Issue not found'
+
+
+def test_update_issue_updates_single_field(client, example):
+    result = client.post('/issues', json = example)
+
+    result_id = result.json()['id']
+
+    response = client.patch(f'/issues/{result_id}', json={'status': 'Closed'})
+
+    assert response.status_code == 200
+    assert response.json()['status'] == 'Closed'
+
+
+def test_update_issue_updates_in_memory(client, example):
+    result = client.post('/issues', json=example)
+
+    result_id = result.json()['id']
+
+    response = client.patch(f'/issues/{result_id}', json={'status':'Closed'})
+
+    status = client.get(f'/issues/{result_id}').json()['status']
+    expected = 'Closed'
+
+    assert status == expected
